d9b395d9f088550c1b760e95a09e2d30b0439a76
iturkan6 <iturkan6@gmail.com>
2022-08-17 03:07:12 +0400

    Create records

---
+++ src/main/java/com/example/headhunter/CLI_App/models/Employer.java
@@ -0,0 +1,4 @@
+package com.example.headhunter.CLI_App.models;
+
+public record Employer(int id, String name, String surname, String companyName, String worker_profession) {
+}
---
+++ src/main/java/com/example/headhunter/CLI_App/models/Worker.java
@@ -0,0 +1,4 @@
+package com.example.headhunter.CLI_App.models;
+
+public record Worker(int id, String name, String surname, String profession) {
+}
