a343e5a769ddd7295d01cb5cefe277204a27f913
iturkan6 <iturkan6@gmail.com>
2022-08-17 00:25:05 +0400

    Initial commit

---
+++ src/main/java/com/example/headhunter/CLI_App/Main.java
@@ -0,0 +1,13 @@
+package com.example.headhunter.CLI_App;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Hi");
+            int a = scanner.nextInt();
+        }
+    }
+}
---
+++ src/main/java/com/example/headhunter/SpringBoot_App/HeadHunterApplication.java
@@ -0,0 +1,13 @@
+package com.example.headhunter.SpringBoot_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadHunterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeadHunterApplication.class, args);
+	}
+
+}
